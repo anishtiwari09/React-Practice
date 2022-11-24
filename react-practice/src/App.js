d64a4d88397ea4f2935e7acd6b8156d98e20be2d
anishtiwari07 <anish@carveniche.com>
2022-11-24 18:43:22 +0530

    intial commit

---
+++ react-practice/src/App.js
@@ -0,0 +1,11 @@
+import Example1 from './ClassComponent/Example1';
+
+function App() {
+  return (
+    <>
+      <Example1 />
+    </>
+  );
+}
+
+export default App;
---
+++ react-practice/src/ClassComponent/Example1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import React, { Component } from 'react'
+
+export default class Example1 extends Component {
+  render() {
+    return (
+      <div>Example1</div>
+    )
+  }
+}
